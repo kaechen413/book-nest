f7651fd326e6b671b34af27d2d44e63f8c177c29
Jiahui Chen <cjh1171740108@gmail.com>
2025-05-23 14:08:30 +0900

    fixed flatpiickr dates etc

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -4,8 +4,16 @@ import flatpickr from "flatpickr"; // You need to import this to use new flatpic
 // Connects to data-controller="datepicker"
 export default class extends Controller {
   connect() {
+    const bookedPeriods = JSON.parse(this.element.dataset.bookedPeriods || "[]")
+    console.log("Booked periods:", bookedPeriods)
+
+    const bookedRanges = bookedPeriods.map(range => ({
+      from: range[0],
+      to: range[1]
+    }));
     flatpickr(this.element, {
-    mode: "range"
-  });
+      mode: "range",
+      disable: bookedRanges
+    });
   }
 }
